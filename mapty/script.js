d739f518adc975ece6577a82bef5bf088de8e81c
Maria Duarte <mariaduarte061@hotmail.com>
2025-04-28 10:00:35 -0300

    feat: use localStorage

--- mapty/script.js
+++ mapty/script.js
@@ -71,6 +71,9 @@ class App {
 
     constructor(){
         this._getPosition();
+
+        this._getLocalStorage();
+
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElevationField);
         containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -97,6 +100,10 @@ class App {
         }).addTo(this.#map);
 
         this.#map.on('click', this._showForm.bind(this));
+
+        this.#workouts.forEach(work => {
+            this._renderWorkoutMarker(work);
+        })
     }
 
     _showForm(mapE){
@@ -156,6 +163,8 @@ class App {
         this._renderWorkout(workout);
 
         this._hideForm();
+
+        this._setLocalStorage();
     }
 
     _renderWorkoutMarker(workout){
@@ -230,7 +239,28 @@ class App {
             }
         });
 
-        workout.click();
+        //workout.click();
+    }
+
+    _setLocalStorage(){
+        localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+    }
+
+    _getLocalStorage(){
+        const data = JSON.parse(localStorage.getItem('workouts'));
+
+        if(!data) return;
+
+        this.#workouts = data;
+
+        this.#workouts.forEach(work => {
+            this._renderWorkout(work);
+        });
+    }
+
+    reset(){
+        localStorage.removeItem("workouts");
+        location.reload();
     }
 }
 
