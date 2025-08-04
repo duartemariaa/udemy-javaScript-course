f9a4fa64f5a8401d760dc128610d89a76782974e
Maria Duarte <mariaduarte061@hotmail.com>
2025-08-04 19:25:00 -0300

    add helpers and config files

--- forkify/model.js
+++ forkify/model.js
@@ -1,3 +1,5 @@
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
 
 export const state = {
     recipe: {},
@@ -5,9 +7,7 @@ export const state = {
 
 export const loadRecipe = async function(id) {
     try {
-        const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
-        const data = await res.json();
-        if(!res.ok) throw new Error(`${data.message} (${res.status})`)
+        const data = await getJSON(`${API_URL}/${id}`);
 
         const {recipe} = data.data;
         state.recipe = {
@@ -24,6 +24,6 @@ export const loadRecipe = async function(id) {
         console.log(state.recipe);
 
     } catch(err) {
-        console.log(err);
+        console.error(`${err} 💥`);
     }
 }
